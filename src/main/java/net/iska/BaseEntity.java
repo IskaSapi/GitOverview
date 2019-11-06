921243a4e398974e6421fc102e317ca70e5c6dc4
Iskander Sapiyev <iskander.sapiyev@gmail.com>
2019-11-06 16:01:56 +0600

    adding new class BaseEntity

---
+++ src/main/java/net/iska/BaseEntity.java
@@ -0,0 +1,27 @@
+package net.iska;
+
+public class BaseEntity {
+    private Long id;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+
+    boolean isNew(){
+        return (this.id==null);
+    }
+
+}
